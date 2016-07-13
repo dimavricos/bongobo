981b45471122c69e2066ba324f192128b250238f
Dimitris <dim_iliop@msn.com>
2016-07-13 19:46:15 +0300

    social

--- app/views/tracks/playlist_details.js
+++ app/views/tracks/playlist_details.js
@@ -1,26 +1,98 @@
 var BaseView = require('../base');
 var Backbone = require('backbone'); 
 module.exports = BaseView.extend({
-  className: 'playlist_details',
-  tagName: 'div', 
- 
-  events : {
-  	 
-  },
+	className: 'playlist_details',
+	tagName: 'div', 
 
-  initialize : function(){
+	events : {
+		'click .facebook-share-playlist' : 'facebookShare',
+		'click .twitter-share-playlist' : 'twitterShare',
+		'click .google-share-playlist' : 'googleShare'
+	},
 
-},
+	initialize : function(){
 
-  postRender: function() {
-  	 console.log(this)
-      
-  }, 
+	},
+
+	postRender: function() {
+		window.fbAsyncInit = function () {
+			FB.init({
+				appId: '1634138913501926',
+				xfbml: true,
+				version: 'v2.3'
+			});
+		};
+
+		(function (d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) { return; }
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/en_US/sdk.js";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+
+		$("a.google-share-playlist").attr('href', 'https://plus.google.com/share?url=' + document.URL);
+	}, 
 
 
+	facebookShare : function(){
+		FB.ui({
+			method: 'feed',
+			link: document.URL,
+		}, function (response) {
+		});
+	},
+
+	twitterShare : function(){
+		var width = 575,
+		height = 400,
+		left = ($(window).width() - width) / 2,
+		top = ($(window).height() - height) / 2,
+		url = this.href,
+		opts = 'status=1' +
+		',width=' + width +
+		',height=' + height +
+		',top=' + top +
+		',left=' + left;
+
+		window.open(url, 'twitter', opts);
+
+		return false;
+	},
+
+
+
+	googleShare : function(){
+		
+		var width = 600,
+		height = 600,
+		left = ($(window).width() - width) / 2,
+		top = ($(window).height() - height) / 2,
+		url = this.href,
+		opts = 'status=1' +
+		',menu-bar=no' +
+		',toolbar=no,resizable=yes,scrollbars=yes' +
+		',width=' + width +
+		',height=' + height +
+		',top=' + top +
+		',left=' + left;
+
+		window.open(url, '', opts);
+
+		return false;
+	}
+
+	
 
 
 
 
 });
+
+
+
+
+
+
+
 module.exports.id = 'tracks/playlist_details';
